3bbfc2fa06fb8cd2b4b91f36b6a1a633e3ad7659
dennis <dennis@dennis-Rev-1-0.(none)>
2013-03-17 12:22:55 +0100

    auto commit

--- accept_conditions.php
+++ accept_conditions.php
@@ -5,9 +5,8 @@ moduleLoader::includeTemplateCommon('js-toc');
 $options = array ('exclude' => '', 'context' => '#siteclone_conditions');
 jstoc_set_toc($options);
 
-echo $conditions = config::getModuleIni('siteclone_conditions');
+$conditions = config::getModuleIni('siteclone_conditions');
 $conditions = get_filtered_content(array('cosmarkdown'), $conditions);
-echo "ok";
 
 echo "<div id=\"toc\"></div>\n";
 echo '<div id="#siteclone_conditions">';
